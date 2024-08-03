dab672cbb58c71a97e27ba93b1ab57fa3a460b42
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-08-03 16:12:20 +0530

    chore: Update site.webmanifest and environment files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,19 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 })
 export class AppComponent {
   title = 'todos-app';
+
+  meta = inject(Meta);
+
+  constructor() {
+    this.meta.addTag({ name: 'viewport', content: 'width=device-width, initial-scale=1' });
+    this.meta.addTag({ name: 'icon', content: 'image/x-icon', href: 'favicon.ico' });
+    this.meta.addTag({ name: 'canonical', content: 'https://todos-app-manthanank.vercel.app/' });
+    this.meta.addTag({ property: 'og:title', content: 'Todos App' });
+    this.meta.addTag({ name: 'author', content: 'Manthan Ankolekar' });
+    this.meta.addTag({ name: 'keywords', content: 'angular, nodejs, express, mongodb' });
+    this.meta.addTag({ name: 'robots', content: 'index, follow' });
+    this.meta.addTag({ property: 'og:description', content: 'A simple Todos App built using Angular, Node.js, Express, and MongoDB. It allows you to add, delete, and update todos.' });
+    this.meta.addTag({ property: 'og:image', content: 'https://todos-app-manthanank.vercel.app/image.jpg' });
+    this.meta.addTag({ property: 'og:url', content: 'https://todos-app-manthanank.vercel.app/' });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api'
+    apiUrl:
+    window.location.hostname === 'localhost'
+      ? 'http://localhost:3000/api'
+      : 'https://todos-app-x8bw.vercel.app/api',
 };
